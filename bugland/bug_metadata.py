e6c4a8fd70fd3c7a2a025e3d2ea4abca3d641128
Caglar <ca9lar@gmail.com>
2012-05-23 11:15:57 -0400

    Removed the old bugland files.

--- bugland/bug_metadata.py
+++
@@ -1,8 +0,0 @@
-class BugMetadata(object):
-
-    def __init__(self):
-        pass
-
-    def add_metadata(self):
-        pass
-
--- bugland/scene_metadata.py
+++
@@ -1,7 +0,0 @@
-class SceneMetadata(object):
-
-    def __init__(self):
-        pass
-
-    def add_metadata(self):
-        pass
